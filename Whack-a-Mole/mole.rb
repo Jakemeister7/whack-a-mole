16a533c90061aacd1c167945b574ff2b04c76b43
Jakemeister7 <goddard.jocelyn37@gmail.com>
2026-07-23 18:33:50 -0400

    Initialized and drew the mole

--- Whack-a-Mole/mole.rb
+++ Whack-a-Mole/mole.rb
@@ -4,10 +4,17 @@ class WhackaMole < Gosu::Window
 	def initialize
 		super(800, 600)
 		self.caption ="Whack a Mole"
+		@image = Gosu::Image.new('images/mole.png')
+		@x = 200
+		@y = 200
+
 	end
 
+	def draw
+		@image.draw(@x, @y, 1)
+	end
 end
 
-Window = WhackaMole
-Window.show_exceptions
+Window = WhackaMole.new
+Window.show
 
