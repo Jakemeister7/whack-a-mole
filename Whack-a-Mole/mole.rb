dde6880a6e0713881898cd8bafdf7948be9908df
Jakemeister7 <goddard.jocelyn37@gmail.com>
2026-07-25 18:00:17 -0400

    Add mole, add movement and set visibility

--- Whack-a-Mole/mole.rb
+++ Whack-a-Mole/mole.rb
@@ -7,12 +7,30 @@ class WhackaMole < Gosu::Window
 		@image = Gosu::Image.new('images/mole.png')
 		@x = 200
 		@y = 200
-
+		@width = 100
+		@height = 75
+		@velocity_x = 3
+		@velocity_y = 3
+		@visible = 0
 	end
 
 	def draw
-		@image.draw(@x, @y, 1)
+		if @visible > 0
+		@image.draw(@x - @width /2, @y - @height / 2, 1)
+		end
+
 	end
+
+	def update
+		@x += @velocity_x
+		@y += @velocity_y
+		@velocity_x *= -1 if @x + @width /2 > 800 || @x - @width /2 < 0
+		@velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height /2 < 0
+		@visible -= 10
+		@visible = 100 if @visible < -10 && rand < 0.01
+
+	end
+
 end
 
 Window = WhackaMole.new
